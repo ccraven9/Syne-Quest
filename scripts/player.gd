f6687e4942fddc762a9911286175d9710bb1d378
ccraven9 <c.craven1337@gmail.com>
2026-03-03 07:52:59 -0500

    Adding baisc level for inheritance, updated player movement

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,11 +1,11 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+const SPEED = 125
 const JUMP_VELOCITY = -400.0
 
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	# Add the gravity.
 	#if not is_on_floor():
 		#velocity += get_gravity() * delta
@@ -14,12 +14,7 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	#Inputs found in Project -> Project Settings -> Input Map
+	var direction = Input.get_vector("left", "right", "up", "down")
+	velocity = direction * SPEED
 	move_and_slide()
